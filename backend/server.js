c86c743b9a83f4f97263689d7f09d667d393e3bf
Ozioma45 <oziomaegole@gmail.com>
2025-01-17 18:42:05 +0100

    getting the users info from registration, implement form logic for proper input and providing errors in case of wrong input

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { pool } = require("./dbconfig");
 const PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -20,6 +21,35 @@ app.get("/users/login", (req, res) => {
 
 app.get("/users/dashboard", (req, res) => {
   res.render("dashboard", { user: "Conor" });
+});
+
+app.post("/users/register", (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  console.log({
+    name,
+    email,
+    password,
+    password2,
+  });
+
+  let errors = [];
+
+  if (!name || !email || !password || password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password should be at least 6 characters" });
+  }
+
+  if (password != password2) {
+    errors.push({ messgae: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors });
+  }
 });
 
 app.listen(PORT, () => {
